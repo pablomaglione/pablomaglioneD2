1836e380accd83cf7fe5ab031a6cd96d2f2ae94b
pablomaglione <pmaglione83@gmail.com>
2022-07-05 16:33:21 -0300

    Desafio2

---
+++ src/components/NavBar.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+const NavBar = (props) => {
+  console.log(props);
+  return (
+    <ul class="nav nav-pills nav-justified">
+      <li class="nav-item">
+        <a class="nav-link active" aria-current="page" href="#">Active</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Much longer nav link</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Link</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link disabled">Disabled</a>
+      </li>
+    </ul>
+  );
+};
+
+export default NavBar;
